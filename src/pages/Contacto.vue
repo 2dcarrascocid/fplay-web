88e0cc25710fbe23be7590adaaec7a60f0e5d049
dcarrasco <dcarrasco@servel.cl>
2025-12-13 11:52:45 -0300

    neuva imagen y correccion de contacto

--- src/pages/Contacto.vue
+++ src/pages/Contacto.vue
@@ -38,10 +38,17 @@ const form = reactive({
 
 const sendEmail = async () => {
   try {
+    const templateParams = {
+      ...form,
+      name: form.nombre,
+      email: form.correo,
+      message: form.mensaje,
+    };
+
     await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      form,
+      templateParams,
       import.meta.env.VITE_EMAILJS_PUBLIC_KEY
     );
 
