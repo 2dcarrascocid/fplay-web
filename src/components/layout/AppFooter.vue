2a811806529ef24de1f188ef73c897c4307132f4
dcarrasco <dcarrasco@servel.cl>
2025-12-15 12:19:54 -0300

    version

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,9 +1,11 @@
 <template>
   <footer class="container text-center" style="padding: 2rem 0; border-top: 1px solid var(--color-border); margin-top: 2rem;">
     <p>© {{ year }} FAIR-PLAY CHILE — Todos los derechos reservados.</p>
+    Versión {{ version }}
   </footer>
 </template>
 
 <script setup>
 const year = new Date().getFullYear();
+const version = __APP_VERSION__
 </script>
